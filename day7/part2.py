5cb778564ab40f6cfd0110becc5cd75a77f3dbc8
Lorenzo <loryparadise04@gmail.com>
2024-12-07 14:12:09 +0100

    day 7

---
+++ day7/part2.py
@@ -0,0 +1,29 @@
+from itertools import product
+
+with open("input.txt", "r") as f:
+    data = f.read().splitlines()
+
+total = 0
+c = 0
+for line in data:
+    c += 1
+    test, values = line.split(":")
+    test = int(test)
+    values = values.strip().split(" ")
+
+    for combination in product(*(['+*|'] * (len(values) - 1))):
+        res = int(values[0])
+        for i in range(len(combination)):
+            match combination[i]:
+                case '+':
+                    res += int(values[i+1])
+                case '*':
+                    res *= int(values[i+1])
+                case '|':
+                    res = int(str(res) + values[i + 1])
+        if res == test:
+            total += test
+            break
+    print("doing line", c)
+
+print(total)
